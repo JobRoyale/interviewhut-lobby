1b026d5f0f7fef18749bb3649899cec7b59781bc
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-04-08 01:38:19 +0530

    interviewhut

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) =>
-	res.send(`CodeRoyale Lobby Server is up and running`)
+	res.send(`InterviewHut Lobby Server is up and running`)
 );
 
 module.exports = router;
